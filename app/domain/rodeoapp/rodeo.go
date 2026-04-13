e86c8f99a89695aae308dcf393b8f9a71974fbfc
jto05 <julian.to2023@gmail.com>
2026-04-12 17:36:31 -0700

    Implemented syncing all Athletes in prorodeo.com

--- app/domain/rodeoapp/rodeo.go
+++ app/domain/rodeoapp/rodeo.go
@@ -5,6 +5,7 @@ package rodeoapp
 import (
 	"context"
 	"fmt"
+	"sync"
 
 	"github.com/am29/ferdinand/app/domain/prorodeoapp"
 	"github.com/jto05/chute/business/data/store/rodeodb"
@@ -20,6 +21,86 @@ type App struct {
 // New constructs an App.
 func New(log *logger.Logger, store *rodeodb.Store) *App {
 	return &App{log: log, store: store}
+}
+
+// SyncAthletes fetches all contestants and stores result into storage.
+func (a *App) SyncAthletes(ctx context.Context) error {
+	a.log.Info("sync athletes started")
+
+	// shared pool of ids
+	ids := make(chan int, 750) // at size 750 because workers consume as soon as fetched ids collected
+
+	// Part 1: collect all contestantIDs
+	var collectWg sync.WaitGroup // semaphore
+	for _, letter := range "ABCDEFGHIJKLMNOPQRSTUVWXYZ" {
+		collectWg.Add(1) // post semapohre
+
+		go func(letter string) {
+			defer collectWg.Done() // wait semaphore at end of func
+			for idx := 1; ; idx++ {
+
+				// fetch athletes by letter
+				raw, err := prorodeoapp.FetchAthletesByLetter(ctx, letter, 100, idx)
+				if err != nil {
+					a.log.Error("fetch athlete ids", "letter", letter, "error", err)
+					return
+				}
+
+				pageIDs, err := prorodeoapp.ParseAthleteIDs(raw)
+				// NOTE: do i need to handle error in this case?
+				if err != nil || len(pageIDs) == 0 { // return if no more IDs pulled
+					return
+				}
+
+				// add ids to shared pool
+				for _, id := range pageIDs {
+					ids <- id
+				}
+			}
+		}(string(letter))
+	}
+
+	// close ids channel when all letters are done fetching
+	go func() {
+		collectWg.Wait()
+		close(ids)
+	}()
+
+	// part 2: fetch athlete based on id from shared channel and store
+	var fetchWg sync.WaitGroup
+	numOfConsumers := 10
+
+	for range numOfConsumers {
+		fetchWg.Add(1) // post
+
+		go func() {
+			defer fetchWg.Done() // wait
+			for id := range ids {
+				// NOTE: should already stored athletes be skipped? should implement way to update exisitng athletes?
+				if a.store.AthleteExists(id) {
+					continue // for now skip existing athletes
+				}
+
+				// get athlete data
+				raw, err := prorodeoapp.FetchAthlete(ctx, id)
+				if err != nil {
+					a.log.Error("fetch athlete", "id", id, "error", err)
+				}
+
+				// parse athlete data
+				athlete, err := prorodeoapp.ParseAthlete(raw)
+				if err != nil {
+					a.log.Error("parse athlete", "id", id, "error", err)
+				}
+
+				// store athlete
+				a.store.SaveAthlete(athlete)
+
+			}
+		}()
+	}
+
+	return nil
 }
 
 // Sync fetches all completed rodeos in the given date range and persists their
